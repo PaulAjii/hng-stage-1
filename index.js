03da45a23940999f8c8abff9aeed6f278874f2bb
PaulAjii <paulajii134@gmail.com>
2025-02-04 15:05:44 +0100

    Refactor number handling in classify-number API to ensure absolute values are processed

--- index.js
+++ index.js
@@ -18,27 +18,29 @@ app.get('/api/classify-number', async (req, res) => {
 	try {
 		const { number } = req.query;
 
-		if (!number || isNaN(number)) {
+		if (!number || isNaN(+number)) {
 			return res.status(400).json({
 				number: number,
 				error: true,
 			});
 		}
 
-		const response = await fetch(`http://numbersapi.com/${number}/math`);
+		const response = await fetch(
+			`http://numbersapi.com/${Math.abs(+number).toString()}/math`
+		);
 
 		if (!response.ok) {
 			throw new Error(`HTTP error! status: ${response.status}`);
 		}
 
 		res.status(200).json({
-			number: number,
-			is_prime: isPrimeNumber(+number),
-			is_perfect: isPerfectNumber(+number),
-			properties: isArmstrongNum(number)
-				? ['armstrong', isEvenNumber(+number)]
-				: [isEvenNumber(+number)],
-			digit_sum: digitSumNum(number),
+			number: Math.abs(number),
+			is_prime: isPrimeNumber(Math.abs(+number)),
+			is_perfect: isPerfectNumber(Math.abs(+number)),
+			properties: isArmstrongNum(Math.abs(+number).toString())
+				? ['armstrong', isEvenNumber(Math.abs(+number))]
+				: [isEvenNumber(Math.abs(+number))],
+			digit_sum: digitSumNum(Math.abs(+number).toString()),
 			fun_fact: await response.text(),
 		});
 	} catch (error) {
